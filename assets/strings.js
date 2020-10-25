5cf3b4b52855d2e173c6543ebd4fb3aa8f9c2466
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 14:35:49 -0300

    Exercise 2-a

---
+++ assets/strings.js
@@ -0,0 +1,11 @@
+console.log("Strings");
+
+/*
+    Exercise a
+    Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+*/
+
+    var word1 =  "radiumrocket".toUpperCase();
+
+    console.log("Exercise a: "+ word1);
+
--- assets/vars-oper.js
+++ assets/vars-oper.js
@@ -1,29 +1,32 @@
 console.log("Vars and operators");
 
-//Exercise a
 /*
+    Exercise a
     Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
 */
+
     var a = 1;
     var b = 2;
 
     var c = a + b;
     console.log("Exercise a: "+c);
 
-//Exercize b
 /*
+    Exercize b
     Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 */
+
     var d = "hello";
     var e = "world";
 
     var f = d + " " +e;
     console.log("Exercise b: "+ f);
 
-//Exercise c
 /*
+    Exercise c
     Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
 */
+
     var g = "hello".length;
     var h = "world".length;
 
