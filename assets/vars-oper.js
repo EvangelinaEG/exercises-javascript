863d82dca6dfec8b6fcce1c4a46a5f2fbb02258e
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 14:27:25 -0300

    Exercise 1-a

---
+++ assets/vars-oper.js
@@ -0,0 +1,12 @@
+console.log("Vars and operators")
+
+//Exercise a
+/*
+    Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+*/
+    var a = 1;
+    var b = 2;
+
+    var c = a + b;
+    console.log("Exercise a: "+c);
+
