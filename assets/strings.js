d353b8b0ddd8605c3e2974e4f112a4e3f67a2c3e
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 14:38:50 -0300

    Exercise 2-f

--- assets/strings.js
+++ assets/strings.js
@@ -53,3 +53,13 @@ var white = word5.indexOf(" ");
 
 console.log("Exercise e: "+ white);
 
+/* 
+Excercise f
+Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
+*/
+
+var word6 = "developers javascript";
+
+var conver =  (word6.substring(0, 1)).toUpperCase() + (word6.substring(1, word6.indexOf(" "))).toLowerCase() + (word6.substring(word6.indexOf(" "),word6.indexOf(" ")+2)).toUpperCase() + (word6.substring(word6.indexOf(" ")+2, word6.length)).toLowerCase();
+
+console.log("Exercise f: " + conver);
