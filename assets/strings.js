cf0b3bdf8b4357aadb2b8a4575cb2e45fb4ffa57
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 14:36:48 -0300

    Exercise 2-b

--- assets/strings.js
+++ assets/strings.js
@@ -9,3 +9,14 @@ console.log("Strings");
 
     console.log("Exercise a: "+ word1);
 
+/*
+    Excercise b
+    Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
+
+    var word2 = "Evangelina";
+
+    var sub = word2.substring(0, 5);
+
+    console.log("Exercise b: "+ sub);
+
