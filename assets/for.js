5156ec81d31f7d490b02082c4cf9a55adeb634ef
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 17:34:07 -0300

    Exercise 5-d

--- assets/for.js
+++ assets/for.js
@@ -52,3 +52,24 @@ console.log("For");
     }
 
     alert(sentence);
+
+/*
+
+    Exercise 5-d
+    Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número 
+    de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 
+    elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola 
+    del navegador el al array final (utilizar console.log).
+
+*/
+
+    console.log("Exercise 5-d");
+
+    var array = [];
+
+    for(var t=0; t < 10; t++){
+        array.push(t);
+    }
+
+    console.log(array);
+
