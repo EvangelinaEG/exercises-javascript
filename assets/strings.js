d251edd0a8665c3b6e29402a09568e2b5b004fb7
evangelina <evangelina.gomes@ellecktra.com>
2020-10-25 14:38:09 -0300

    Exercise 2-d

--- assets/strings.js
+++ assets/strings.js
@@ -31,3 +31,25 @@ console.log("Strings");
 
     console.log("Exercise c: "+ sub1);
 
+/*
+    Exercise d
+    Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+*/
+
+var word4 = "evangelina";
+
+var toUplo = (word4.substring(0, 1)).toUpperCase() + (word4.substring(1,word4.length)).toLowerCase();
+
+console.log("Exercise d: "+ toUplo);
+
+/*
+Exercise e
+Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+*/
+
+var word5 = "Radium Rocket";
+
+var white = word5.indexOf(" ");
+
+console.log("Exercise e: "+ white);
+
